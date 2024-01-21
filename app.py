5c6f07086eb768fc5b06e7edf1b0941fd813ae35
Sonia Majewska <sonianltran@gmail.com>
2024-01-21 16:47:54 +0000

    Added functionality to register

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env
@@ -17,7 +18,33 @@ def home():
 
 @app.route("/register")
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+
+def login():
+    return render_template("login.html")
 
 
 @app.route("/sign_in")
